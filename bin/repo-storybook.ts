5225271f562abc2d584cd16d0489be54e2c0487c
Arthur (@Refzlund) <arthur@refzlund.com>
2025-06-17 00:39:46 +0200

    Handle storybook duplicate port

--- bin/repo-storybook.ts
+++ bin/repo-storybook.ts
@@ -2,6 +2,36 @@ import { fileURLToPath, spawn } from 'bun'
 import ora from 'ora'
 import 'colors'
 import path from 'path'
+import readline from 'node:readline'
+import { parseArgs } from 'util'
+
+function keypress() {
+	return new Promise<string>((resolve) => {
+		process.stdin.setRawMode(true)
+		process.stdin.setEncoding('utf8')
+
+		process.stdin
+			.once('data', data => {
+				process.stdin.pause()
+				resolve(`${data || ''}`)
+			})
+			.resume()
+	})
+} 
+
+const { values } = parseArgs({
+	args: Bun.argv,
+	options: {
+		// --normal
+		/** Do not use fancy formatting when running Storybook */
+		normal: {
+			type: 'boolean',
+			short: 'n',
+			default: false
+		}
+	},
+	allowPositionals: true
+})
 
 const storybookConfigDir = path.join(fileURLToPath(import.meta.url), '../../.storybook')
 const subprocess = spawn({
@@ -9,7 +39,8 @@ const subprocess = spawn({
 		...`bun --bun storybook dev -p 6006 --no-open --disable-telemetry --config-dir`.split(' '),
 		storybookConfigDir
 	],
-	stdout: 'pipe'
+	stdout: 'pipe',
+	stdin: 'pipe'
 })
 
 let version = '0.0.0'
@@ -17,30 +48,37 @@ let text = '...'
 
 const storybook = 'Storybook'.yellow.bold as unknown as string
 
+const book = {
+	interval: 100,
+	frames: [
+		'⎺',
+		'⎻',
+		'⎼',
+		'⎽',
+		'⎼',
+		'⎽',
+		'⎽',
+		'⦟',
+		'∠',
+		'∟',
+		'⦦',
+		'⎽',
+		'⎽',
+		'⎽'.gray,
+		'⎽'.gray.italic
+	]
+}
+
 const spinner = ora({
 	text: ` ${storybook} ${version} starting: ${text}`,
-	color: 'magenta',
-	spinner: {
-		interval: 100,
-		frames: [
-			'⎺'.yellow,
-			'⎻'.yellow,
-			'⎼'.yellow,
-			'⎽'.yellow,
-			'⎼'.yellow,
-			'⎽'.yellow,
-			'⎽'.yellow,
-			'⦟'.yellow,
-			'∠'.yellow,
-			'∟'.yellow,
-			'⦦'.yellow,
-			'⎽'.yellow,
-			'⎽'.yellow,
-			'⎽'.gray,
-			'⎽'.gray.italic
-		]
-	}
-}).start()
+	color: 'yellow',
+	discardStdin: false,
+	spinner: book
+})
+
+if(!values.normal) {
+	spinner.start()
+}
 
 console.clear()
 console.log('')
@@ -53,11 +91,53 @@ function removeFormat(str: string) {
 const decoder = new TextDecoder()
 const warnings = [] as string[]
 for await (const line of subprocess.stdout) {
+	if(values.normal) {
+		console.log(decoder.decode(line))
+		const port = decoder.decode(line).match(/Port (\d+) is not available/im)
+		if(port) {
+			const key = await keypress()
+			subprocess.stdin.write(`${key}\n`)
+			subprocess.stdin.flush()
+			subprocess.stdin.end()
+		}
+		continue
+	}
+
 	const str = removeFormat(decoder.decode(line))
 	
-	const err = str.match(/Original error:\s*\n+((.|\n)+)$/im)
+	const port = str.match(/\? Port (\d+) is not available/im)
+	if(port) {
+		const alt = str.match(/port (\d+) instead/im) || []
+		// »
+		spinner.spinner = 'arc'
+		spinner.color = 'blue'
+		spinner.text = ` ${storybook} ${version}: ` + `Port ${port[1].blue} is not available` + ' — '.gray + `Use port ${alt[1].blue} instead?` + ' ⩺ (Y/n)'.gray
+		
+		let key: string | undefined
+		do {
+			key = await keypress()
+		} while(['y', 'n', ''].includes(key.toLowerCase()) === false)
+
+		if(key === 'n') {
+			spinner.fail(` ${storybook} ${version} ` + `was cancelled.`.red)
+			process.exit(1)
+		}
+
+		spinner.spinner = book
+		spinner.color = 'yellow'
+
+		spinner.text = ` ${storybook} ${version}: ${'✓'.green} Using port ${alt[1].blue} instead`
+
+		subprocess.stdin.write('y\n')
+		subprocess.stdin.flush()
+		subprocess.stdin.end()
+
+		await new Promise(resolve => setTimeout(resolve, 1000))
+	}
+
+	const err = str.match(/(Original error:\s*\n+((.|\n)+)$)|(Error: ((.|\n)+)$)/im)
 	if(err) {
-		spinner.fail(` ${storybook} ${version} failed:`.red + `\n${err[1]}`)
+		spinner.fail(` ${storybook} ${version} ${'failed'.red}:\n${err[2] ?? err[4]}`)
 		process.exit(1)
 	}
 
